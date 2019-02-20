dc85b0530d24ec6164236d095c8919eba9730cde
Brendan Berejikian <bezoric1@gmail.com>
2019-02-20 12:01:52 -0500

    Update Program.cs

--- Volume of a hemisphere/Program.cs
+++ Volume of a hemisphere/Program.cs
@@ -6,10 +6,25 @@ using System.Threading.Tasks;
 
 namespace Volume_of_a_hemisphere
 {
-    class Program
-    {
-        static void Main(string[] args)
+    class HemisphereVolume
         {
-        }
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Hemisphere Radius: ");
+                double radius = double.Parse(Console.ReadLine());
+                double pi = Math.PI;
+            double tt = (4/3);
+                double volume =  (tt) * (pi * (radius * radius * radius));
+
+               double volume2 = volume/2;
+
+                Console.WriteLine("Volume = " + volume2);
+                Console.ReadKey();
+
+            }
     }
+
+        
 }
+    
+
